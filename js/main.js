45f14c3f60c6875033cbc56eb9219801ef4f4bb7
Aviv Gibli <avivgib1012@gmail.com>
2024-12-29 04:41:58 +0200

    Initialize gallery button to active

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 'use strict'
 
+document.addEventListener('DOMContentLoaded', () => {
+    onInit();
+})
+
 function onInit() {
     renderGallery()
-    showSection('gallery-section', document.querySelector('[data-section="gallery-section"]'));
+    const galleryButton = document.querySelector('.gallery')
+
+    if (galleryButton) {
+        galleryButton.classList.add('active')
+        showSection('gallery-section', galleryButton)
+    }
 }
 
 function showSection(sectionClass, clickedButton) {
     document.querySelectorAll('section').forEach(section => section.classList.add('hidden'))
-    document.querySelector(`.${sectionClass}`).classList.remove('hidden')
+    // document.querySelector(`.${sectionClass}`).classList.remove('hidden')
     
+    const sectionToShow = document.querySelector(`.${sectionClass}`)
+    if (sectionToShow) {
+        sectionToShow.classList.remove('hidden')
+    }
+
     document.querySelectorAll('nav button').forEach(btn => btn.classList.remove('active'))
     if (clickedButton) {
         clickedButton.classList.add('active')
